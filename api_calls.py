3284e277d1b56a062ae5bfecce2a9989997e444c
tcurtis <tony.curtis@eplus.com>
2024-02-08 11:48:36 -0500

    Added test_function

--- api_calls.py
+++ api_calls.py
@@ -4,6 +4,9 @@ from requests.auth import HTTPDigestAuth
 
 # This is a test
 
+def test_function():
+    pass
+
 def test_get_with_basic_auth():
     url = 'https://httpbin.org/basic-auth/user/password'
     auth = requests.auth.HTTPBasicAuth('user', 'password')
